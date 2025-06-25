1042201ef965790deabf3de3cb566d56ceb7e0f6
itsuki <itsuki.enjyoy@gmail.com>
2025-06-26 08:28:59 +0900

    Initial Commit

---
+++ MediaPlayerDemo/MediaPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerDemoApp.swift
+//  MediaPlayerDemo
+//
+//  Created by Itsuki on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
